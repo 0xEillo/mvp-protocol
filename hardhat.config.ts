ef062f7694f97cbbd51bf1e35ebd492f4a22e154
Oliver Ryall <oliver@olivers-mbp.home>
2022-06-17 10:46:32 +0200

    ⚡clean: hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,16 +4,6 @@ import "@nomiclabs/hardhat-ethers";
 const GOERLI_API_KEY = process.env.GOERLI_API_KEY;
 const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
 
-// Go to https://www.alchemyapi.io, sign up, create
-// a new App in its dashboard, and replace "KEY" with its key
-const ALCHEMY_API_KEY = "KEY";
-
-// Replace this private key with your Goerli account private key
-// To export your private key from Metamask, open Metamask and
-// go to Account Details > Export Private Key
-// Be aware of NEVER putting real Ether into testing accounts
-const ROPSTEN_PRIVATE_KEY = "YOUR ROPSTEN PRIVATE KEY";
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
